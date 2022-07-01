e1cecd85db91309d8b75245628b828a9d914c3ec
valkarov <88056645+valkarov@users.noreply.github.com>
2022-06-30 19:01:24 -0600

    Examen Desarrollo Web

--- ExamenWeb/src/main/java/com/IExamenWeb/modelo/Persona.java
+++ ExamenWeb/src/main/java/com/IExamenWeb/modelo/Persona.java
@@ -1,10 +1,47 @@
-
 package com.IExamenWeb.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "persona")
 
 public class Persona {
     
-    private int id;
-    private String name;
-    private String telefono;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+private int id;
+private String name;
+private String telefono;
+
+public Persona() {
     
 }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
+
